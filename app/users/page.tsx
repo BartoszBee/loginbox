28f3b98a49704416af19d31afef37b9ba540cad1
bartoszb <b.biernacki@delta.poznan.pl>
2025-11-25 15:00:14 +0100

    delete user

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,20 @@
 import { db } from "@/lib/db";
 import Link from "next/link";
+import { revalidatePath } from "next/cache";
 
 export default function UsersPage() {
+  // ---- SERVER CARD ACTION ----
+  async function deleteUserAction(formData: FormData) {
+    "use server";
+
+    const id = Number(formData.get("id"));
+    const stmt = db.prepare("DELETE FROM users WHERE id = ?");
+    stmt.run(id);
+
+    revalidatePath("/users")
+  }
+
+  // ---- SELECT ALL USERS ----
   const stmt = db.prepare(
     "SELECT id, email, created_at FROM users ORDER BY id ASC"
   );
@@ -14,18 +27,25 @@ export default function UsersPage() {
 
   return (
     <main className="min-h-screen p-8 bg-gray-50 text-center">
-      <h1 className="text-2xl font-bold mb-2 text-center mx-auto">Lista użytkowników</h1>
-      <Link href="/" className="text-blue-600 hover:underline mx-auto inline-block mb-3 mt-4">
-          ← Powrót do logowania
-        </Link>
+      <h1 className="text-2xl font-bold mb-2 text-center mx-auto">
+        Lista użytkowników
+      </h1>
 
-      <div className="overflow-x-auto  max-w-3xl mx-auto">
+      <Link
+        href="/"
+        className="text-blue-600 hover:underline mx-auto inline-block mb-3 mt-4"
+      >
+        ← Powrót do logowania
+      </Link>
+
+      <div className="overflow-x-auto max-w-3xl mx-auto">
         <table className="min-w-full bg-white border rounded-lg shadow">
           <thead className="bg-blue-600 border-b text-white">
             <tr>
               <th className="px-4 py-2 text-left font-medium">ID</th>
               <th className="px-4 py-2 text-left font-medium">Email</th>
               <th className="px-4 py-2 text-left font-medium">Data utworzenia</th>
+              <th className="px-4 py-2 text-left font-medium">Usuń</th>
             </tr>
           </thead>
 
@@ -33,7 +53,7 @@ export default function UsersPage() {
             {users.length === 0 && (
               <tr>
                 <td
-                  colSpan={3}
+                  colSpan={4}
                   className="px-4 py-4 text-center text-gray-500"
                 >
                   Brak użytkowników.
@@ -46,6 +66,17 @@ export default function UsersPage() {
                 <td className="px-4 py-2">{u.id}</td>
                 <td className="px-4 py-2">{u.email}</td>
                 <td className="px-4 py-2">{u.created_at}</td>
+                <td className="px-4 py-2 text-center">
+                  <form action={deleteUserAction}>
+                    <input type="hidden" name="id" value={u.id} />
+                    <button
+                      type="submit"
+                      className="px-3 py-1 text-sm bg-red-600 text-white rounded hover:bg-red-700"
+                    >
+                      Usuń
+                    </button>
+                  </form>
+                </td>
               </tr>
             ))}
           </tbody>
